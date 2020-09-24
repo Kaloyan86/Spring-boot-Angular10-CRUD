b36a150d45312d5767f90f0efbffb717b5252c96
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2020-09-24 22:16:52 +0300

    add exceptions/ResourceNotFoundException.java, EmployeeRepository.java

--- spring-boot-backend/src/main/java/com/springbackend/repositories/EmployeeRepository.java
+++ spring-boot-backend/src/main/java/com/springbackend/repositories/EmployeeRepository.java
@@ -1,4 +1,10 @@
 package com.springbackend.repositories;
 
-public interface EmployeeRepository {
+import com.springbackend.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
