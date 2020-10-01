8cdef3cbf6b9162f7a2e38b533e20fec2df39187
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2020-10-01 23:34:58 +0300

    add @CrossOrigin annotation in EmployeeController

--- spring-boot-backend/src/main/java/com/springbackend/controllers/EmployeeController.java
+++ spring-boot-backend/src/main/java/com/springbackend/controllers/EmployeeController.java
@@ -5,12 +5,13 @@ import com.springbackend.models.Employee;
 import com.springbackend.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @Controller
 @RequestMapping("/api/v1")
 public class EmployeeController {
