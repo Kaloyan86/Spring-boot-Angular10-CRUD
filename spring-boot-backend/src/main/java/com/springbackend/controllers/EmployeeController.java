7b8a76dadc9f753bed522f18029ae6b73a7d4539
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2020-10-09 00:26:54 +0300

    add update employee rest api

--- spring-boot-backend/src/main/java/com/springbackend/controllers/EmployeeController.java
+++ spring-boot-backend/src/main/java/com/springbackend/controllers/EmployeeController.java
@@ -16,25 +16,24 @@ import java.util.List;
 @RequestMapping("/api/v1")
 public class EmployeeController {
 
+
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    // get all employees
-
+    // get all employees rest api
     @GetMapping("/employees")
     @ResponseBody
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
     }
 
-    // create employee
+    // create employee rest api
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return this.employeeRepository.save(employee);
     }
 
-    // get employee by id
-
+    // get employee by id rest api
     @GetMapping("/employees/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
 
@@ -42,5 +41,19 @@ public class EmployeeController {
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("An employee with id: %d does not exist!", id)));
 
         return ResponseEntity.ok(employee);
+    }
+
+    // update employee rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = this.employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("An employee with id: %d does not exist!", id)));
+
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setEmailId(employeeDetails.getEmailId());
+
+        Employee updatedEmployee = this.employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
     }
 }
