6a2e1ea391272fa97ee16f7bd39550d9d3cb5cc8
Kaloyan Kostadinov <kaloqn_kostadinow@abv.bg>
2021-07-12 12:41:23 +0300

    removed SNAPSHOT from pom.xml changed port to 9000 in EmployeeController added MYSQLHOST environment variable in application.properties

--- spring-boot-backend/src/main/java/com/springbackend/controllers/EmployeeController.java
+++ spring-boot-backend/src/main/java/com/springbackend/controllers/EmployeeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:9000")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
